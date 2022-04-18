fceeb989f4c83e7cc2ec9e7ab1a75ee1a193d989
fake20747 <99074770+fake20747@users.noreply.github.com>
2022-04-18 05:57:37 +0300

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.sql.*;
+
+
+class MainClass{
+	public static void main() throws SQLException {
+String key = "gb09ym9ydoolp3w886d0tciczj6ve9kszqd65u7d126040gwy86xqimjpuuc788g";
+ SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("test.db", key, null) 
+}	}
