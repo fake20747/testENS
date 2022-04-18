de236243bbb98f843f07a5fe9b4eb9a129451728
fake20747 <99074770+fake20747@users.noreply.github.com>
2022-04-18 06:12:19 +0300

    Create tt.java

---
+++ tt.java
@@ -0,0 +1,20 @@
+import javax.crypto.Cipher;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.NullCipher;
+
+
+class MainClass{
+	public static void main() throws NoSuchAlgorithmException, NoSuchPaddingException {
+try  {
+   Cipher robustCipher1 = Cipher.getInstance("DES");
+   Cipher robustCipher7 = Cipher.getInstance("DESede");
+   Cipher robustCipher13 = Cipher.getInstance("RC2");
+   Cipher robustCipher19 = Cipher.getInstance("RC4");
+   Cipher robustCipher25 = Cipher.getInstance("Blowfish");
+   NullCipher ncRobust = new NullCipher();
+   }
+      catch(NoSuchAlgorithmException|NoSuchPaddingException e){
+   }
+ 
+}	}
