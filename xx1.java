ac3a8a0d0776845b97ee64d681ea4b1c99a8b9c8
fake20747 <99074770+fake20747@users.noreply.github.com>
2022-12-29 14:20:18 +0300

    Create xx1.java

---
+++ xx1.java
@@ -0,0 +1,46 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try ( PrintWriter out = response.getWriter() ) 
+        {
+            String userName = request.getParameter("userName");
+            String userPass = request.getParameter("userPass");
+
+            Process pr = Runtime.getRuntime().exec("ping www.stackabuse.com");
+            Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"" + userName +"\"");
+
+        } catch (IOException ex) {
+            Logger.getLogger(MyServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
