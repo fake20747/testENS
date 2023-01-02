d481e2a299083670c8138f8825142319e6608532
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 18:09:27 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,62 @@
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try ( PrintWriter out = response.getWriter() ) 
+        {
+            String userName = request.getParameter("userName");
+            String userPass = request.getParameter("userPass");
+            
+            userPass = "password";
+            
+            InputStream ins = getServletContext().getResourceAsStream("/etc/" + userName);
+
+            if (ins != null) {
+                InputStreamReader isr = new InputStreamReader(ins);
+                BufferedReader reader = new BufferedReader(isr);
+                String word = "";
+                while ((word = reader.readLine()) != null) {
+                    out.println(word);
+                }
+            }
+            else
+            {
+                out.println("No such file exists !");
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MyServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
