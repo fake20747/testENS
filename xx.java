51d72ae0c59f761d97a90e3d0f8d940a7bde4d11
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 21:26:20 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+
+class MainClass{
+	public static void main() throws IOException  {
+String fileName = null;
+FileOutputStream fos = new FileOutputStream (fileName , true);
+ ObjectOutputStream out = new ObjectOutputStream(fos);
+ 
+}	}
