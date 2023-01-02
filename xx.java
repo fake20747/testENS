2dae55e21e63d714b2781715c796715a373d00f1
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 19:44:20 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+class MainClass{
+	public static void main() throws  {
+ Calendar calendar = Calendar.getInstance();
+ int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+ if(currentHour >= 8 && currentHour <= 19) {
+ /* access */ }
+ else {
+ /* deny */}
+}
+ 
+}	}
