74a6c9cc1b70ecd1e2aed0e081700d792a9fb291
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-03 00:34:29 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,10 @@
+class MainClass{
+
+ class MainClass{ 
+	 public static void main(String[] args) {
+void vfkroe() throws ClassNotFoundException {
+         String className = System.getProperty("messageClassName");
+         Class clazz = Class.forName(className);
+     }
+ 
+}	}
