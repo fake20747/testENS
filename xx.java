e45adb3516758160943307a7394b658f91ef7f2f
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 19:10:25 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,31 @@
+JSP Code:
+<html>
+ <head>
+ <body>
+ <title>Vulnerable Page</title>
+ <form name="Vulnerable Form" method="POST" action"maliciousServlet">
+ <label> Enter Text: </label>
+ <input type="text" id="tBox">
+ <input type="submit">
+ </form>
+ </body>
+ </html>
+
+Java Servlet:
+package net.codejava.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/maliciousServlet")
+ public class MaliciousServlet extends HttpServlet {
+ protected void doPost(HttpServletRequest request, HttpServletResponse response) throwsServletException, IOException {
+String file = request.getParameter("tbox");
+ String dir = getServletContext().getRealPath("/banking");
+ File f = new File((dir + "\" + file).replaceAll("\\", "/"));
+ 
+}	}
