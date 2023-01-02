fb4ae598ea3f5e3682747ce070ef6ed6d85a23b8
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-03 00:44:47 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.System.Logger;
+
+
+class MainClass{
+	public static void main() {
+Object req;
+String param1 = ((Object) req).getParameter("param1");
+ Logger.info("Param1: " + param1 + " " + Logger.getName());
+ 
+}	}
