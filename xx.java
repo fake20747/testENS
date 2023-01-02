d384277c796952fca74b7d093848a2ed5ab7a7a6
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 18:53:44 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,63 @@
+import com.sun.net.httpserver.HttpExchange;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+        
+public class MyServlet extends HttpServlet {
+    
+    static Logger logger = Logger.getLogger(MyServlet.class);
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, InterruptedException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try ( PrintWriter out = response.getWriter() ) 
+        { 
+            Long time = Long.parseLong(request.getParameter("time"));
+            Thread.sleep(time);
+  
+        } catch (IOException ex) {
+            //Logger.getLogger(MyServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (InterruptedException ex) {
+            java.util.logging.Logger.getLogger(MyServlet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (InterruptedException ex) {
+            java.util.logging.Logger.getLogger(MyServlet.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
