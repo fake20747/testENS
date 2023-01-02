6ed03ac302054d1abde8776b2d4917aef0560209
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 21:17:06 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.sql.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+
+class MainClass{
+	public static void main() throws SQLException {
+factory.setFeature("http://apache.org/xml/features/xinclude", true);
+ 
+}	}
