cb37cc5f507692b8dd6457603bc9eafed59c8f75
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 21:31:24 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+class MainClass{
+	public static void main() throws StreamCorruptedException {
+float a = 16777216.0f;
+ float b = 1.0f;
+ float c = a + b;
+ double d = a + b;
+ 
+}	}
