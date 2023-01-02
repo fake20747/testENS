8dd6e51c31f5a5f4c7cc1c232ec05af76a06caf0
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 19:50:14 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+
+class MainClass{
+	public static void main() throws  {
+java.util.regex.Pattern.compile("(a+)+").matcher("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "aaaaaaaaaaaaaaa!").matches();
+ 
+}	}
