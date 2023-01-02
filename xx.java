45cd6636fec5e62cd6f7d1be986a9a96a04c9287
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-02 20:01:45 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+
+class MainClass{
+	public static void main() throws IOException, NoSuchAlgorithmException {
+SSLContext context = SSLContext.getInstance("TLSv1.1");
+ 
+}	}
