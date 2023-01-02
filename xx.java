4893570170c0bea8b8f6af75b59b2ebe35814d54
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-03 00:31:19 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,18 @@
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.X509TrustManager;
+
+
+class MainClass{
+	public static void main() throws CertificateException {
+class TrustAllManager implements X509TrustManager {
+          @Override         public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+         }
+          @Override         public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+         }
+          @Override         public X509Certificate[] getAcceptedIssuers() {
+             return null;
+         }
+     }
+ 
+}	}
