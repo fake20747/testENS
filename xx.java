3f6bc5a5dde5d46821801fe13fbab64abb39f0c1
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-03 08:25:20 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,110 @@
+import testcasesupport.*;
+
+import javax.servlet.http.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.util.logging.Level;
+
+import java.sql.*;
+
+
+public class uygasjhd extends AbstractTestCase
+{
+    
+    public void oiiouhjk() throws Throwable
+    {
+        String dataCopy;
+        {
+            String data;
+
+            data = ""; 
+
+            
+            {
+                URLConnection urlConnection = (new URL("http://www.example.org/")).openConnection();
+                BufferedReader readerBuffered = null;
+                InputStreamReader readerInputStream = null;
+
+                try
+                {
+                    readerInputStream = new InputStreamReader(urlConnection.getInputStream(), "UTF-8");
+                    readerBuffered = new BufferedReader(readerInputStream);
+
+                    
+                    
+                    data = readerBuffered.readLine();
+                }
+                catch (IOException exceptIO)
+                {
+                    IO.logger.log(Level.WARNING, "Error with stream reading", exceptIO);
+                }
+                finally
+                {
+                    
+                    try
+                    {
+                        if (readerBuffered != null)
+                        {
+                            readerBuffered.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing BufferedReader", exceptIO);
+                    }
+
+                    try
+                    {
+                        if (readerInputStream != null)
+                        {
+                            readerInputStream.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing InputStreamReader", exceptIO);
+                    }
+                }
+            }
+
+            dataCopy = data;
+        }
+        {
+            String data = dataCopy;
+
+            Connection dbConnection = null;
+
+            try
+            {
+                dbConnection = IO.getDBConnection();
+
+                
+                dbConnection.setCatalog(data);
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error getting database connection", exceptSql);
+            }
+            finally
+            {
+                try
+                {
+                    if (dbConnection != null)
+                    {
+                        dbConnection.close();
+                    }
+                }
+                catch (SQLException exceptSql)
+                {
+                    IO.logger.log(Level.WARNING, "Error closing Connection", exceptSql);
+                }
+            }
+
+        }
+    }
+}
