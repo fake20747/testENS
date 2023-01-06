9b3d8e6f686c060df11d05961b7f966a77c75cad
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 22:16:55 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,89 @@
+import testcasesupport.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.logging.Level;
+
+public class opqwiepold extends AbstractTestCase
+{
+    
+    private boolean privateTrue = true;
+    private boolean privateFalse = false;
+
+    
+    public void mmcmxcep() throws Throwable
+    {
+        int data;
+        if (privateTrue)
+        {
+            data = Integer.MIN_VALUE; 
+            {
+                InputStreamReader readerInputStream = null;
+                BufferedReader readerBuffered = null;
+                
+                try
+                {
+                    readerInputStream = new InputStreamReader(System.in, "UTF-8");
+                    readerBuffered = new BufferedReader(readerInputStream);
+                    
+                    String stringNumber = readerBuffered.readLine();
+                    if (stringNumber != null) 
+                    {
+                        try
+                        {
+                            data = Integer.parseInt(stringNumber.trim());
+                        }
+                        catch(NumberFormatException exceptNumberFormat)
+                        {
+                            IO.logger.log(Level.WARNING, "Number format exception parsing data from string", exceptNumberFormat);
+                        }
+                    }
+                }
+                catch (IOException exceptIO)
+                {
+                    IO.logger.log(Level.WARNING, "Error with stream reading", exceptIO);
+                }
+                finally
+                {
+                    try
+                    {
+                        if (readerBuffered != null)
+                        {
+                            readerBuffered.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing BufferedReader", exceptIO);
+                    }
+
+                    try
+                    {
+                        if (readerInputStream != null)
+                        {
+                            readerInputStream.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing InputStreamReader", exceptIO);
+                    }
+                }
+            }
+            
+        }
+        else
+        {
+            
+            data = 0;
+        }
+
+        {
+            
+            IO.writeLine((byte)data);
+        }
+
+    }
+}
