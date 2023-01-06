c1a6ee76312eb7ecfe11d30e1ee297b26cbb8b1f
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 20:57:25 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,14 @@
+import testcasesupport.*;
+
+public class kajsldkj extends AbstractTestCase
+{
+    public void poiwabms() throws Throwable
+    {
+        if (5 == 5)
+        {
+            String libraryName = "test.dll";
+            
+            System.loadLibrary(libraryName);
+        }
+    }
+}
