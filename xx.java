6ed7e0cce5210543a828af48b1e96c5835056d71
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 22:13:11 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,19 @@
+import testcasesupport.*;
+
+public class poqwjkjds extends AbstractTestCase
+{
+    public void uweygr() throws Throwable
+    {
+        if (IO.staticTrue)
+        {
+            int[] intArray = new int[10];
+            int i = 0;
+            do
+            {
+                IO.writeLine("intArray[" + i + "] = " + (intArray[i] = i));
+                i++;
+            }
+            while (i <= intArray.length);   
+        }
+    }
+}
