4b00f3a8057953afd9489bbe1724c5566a7c700c
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 21:16:01 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,80 @@
+import testcasesupport.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.util.logging.Level;
+
+public class lkjasdfue extends AbstractTestCase
+{
+    public void lkaspowe() throws Throwable
+    {
+        String data;
+        if (true)
+        {
+            data = ""; 
+            
+            {
+                URLConnection urlConnection = (new URL("http://www.example.org/")).openConnection();
+                BufferedReader readerBuffered = null;
+                InputStreamReader readerInputStream = null;
+                try
+                {
+                    readerInputStream = new InputStreamReader(urlConnection.getInputStream(), "UTF-8");
+                    readerBuffered = new BufferedReader(readerInputStream);
+                    
+                    
+                    data = readerBuffered.readLine();
+                }
+                catch (IOException exceptIO)
+                {
+                    IO.logger.log(Level.WARNING, "Error with stream reading", exceptIO);
+                }
+                finally
+                {
+                    
+                    try
+                    {
+                        if (readerBuffered != null)
+                        {
+                            readerBuffered.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing BufferedReader", exceptIO);
+                    }
+
+                    try
+                    {
+                        if (readerInputStream != null)
+                        {
+                            readerInputStream.close();
+                        }
+                    }
+                    catch (IOException exceptIO)
+                    {
+                        IO.logger.log(Level.WARNING, "Error closing InputStreamReader", exceptIO);
+                    }
+                }
+            }
+        }
+        else
+        {
+            
+            data = null;
+        }
+
+        if (true)
+        {
+            if (data != null)
+            {
+                
+                System.out.printf(data);
+            }
+        }
+    }
+}
