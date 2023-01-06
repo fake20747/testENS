66079efea2aee965b1965427613ea490b474990b
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 23:02:44 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,30 @@
+import testcasesupport.*;
+
+import java.security.SecureRandom;
+
+public class kashdpqw extends AbstractTestCase
+{
+    
+    public void opkpokld() throws Throwable
+    {
+        int data;
+        if (IO.staticReturnsTrueOrFalse())
+        {
+            
+            data = (new SecureRandom()).nextInt();
+        }
+        else
+        {
+
+            
+            data = 2;
+
+        }
+
+        {
+            
+            IO.writeLine((byte)data);
+        }
+
+    }
+}
