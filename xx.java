0cbda52316adf9f2669f75b36a9a1a456c7432eb
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 22:11:38 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,26 @@
+import testcasesupport.*;
+
+public class oiqhwkjd extends AbstractTestCase
+{
+    public void mkjasdpe() throws Throwable
+    {
+        short data;
+        if (IO.staticReturnsTrue())
+        {
+            
+            data = Short.MIN_VALUE;
+        }
+        else
+        {
+            
+            data = 0;
+        }
+
+        if(IO.staticReturnsTrue())
+        {
+            
+            short result = (short)(data - 1);
+            IO.writeLine("result: " + result);
+        }
+    }
+}
