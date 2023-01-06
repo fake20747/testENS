f88c85c94cd21c5b6dc9f625b90ba2db0627f24b
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 21:12:53 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,29 @@
+import testcasesupport.*;
+
+public class lkajsdhf extends AbstractTestCase
+{
+    public void kncijoae() throws Throwable
+    {
+        String data;
+        if (IO.staticTrue)
+        {
+            
+            
+            data = System.getenv("ADD");
+        }
+        else
+        {
+            
+            data = null;
+        }
+
+        if (IO.staticTrue)
+        {
+            if (data != null)
+            {
+                
+                System.out.printf(data);
+            }
+        }
+    }
+}
