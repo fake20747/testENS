ffcad6944072ccd89c7591a685bac6aff52ab196
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 21:06:07 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,29 @@
+import testcasesupport.*;
+
+import javax.servlet.http.*;
+
+public class ahsdoieo extends AbstractTestCase
+{
+    public void lkajsdpe() throws Throwable
+    {
+        int data;
+
+        
+        data = 100;
+
+        
+        int array[] = { 0, 1, 2, 3, 4 };
+
+        
+        if (data >= 0)
+        {
+            IO.writeLine(array[data]);
+        }
+        else
+        {
+            IO.writeLine("Array index out of bounds");
+        }
+
+    }
+}
+
