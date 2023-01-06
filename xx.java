5c39252f30939354c824e3089f13b46764a838dd
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 22:14:25 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,17 @@
+import testcasesupport.*;
+
+public class oqiwjdmls extends AbstractTestCase
+{
+    public void mnxpepls() throws Throwable
+    {
+        if (IO.STATIC_FINAL_FIVE == 5)
+        {
+            int[] intArray = new int[10];
+            
+            for (int i = 0; i <= intArray.length; i++)
+            {
+                IO.writeLine("intArray[" + i + "] = " + (intArray[i] = i));
+            }
+        }
+    }
+}
