8dfd84af5aa19ead4c88cf4e349115c1b1ade893
fake20747 <99074770+fake20747@users.noreply.github.com>
2023-01-06 22:15:36 +0300

    Create xx.java

---
+++ xx.java
@@ -0,0 +1,19 @@
+import testcasesupport.*;
+
+public class mmmnasdpe extends AbstractTestCase
+{
+    public void poqweilk() throws Throwable
+    {
+        if (IO.STATIC_FINAL_FIVE == 5)
+        {
+            int[] intArray = new int[10];
+            int i = 0;
+            
+            while (i <= intArray.length)
+            {
+                IO.writeLine("intArray[" + i + "] = " + (intArray[i] = i));
+                i++;
+            }
+        }
+    }
+}
